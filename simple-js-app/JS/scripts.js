ef8cf6b85b452b4e2c9a00a5e1ffdd04c63a83de
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-06 15:39:27 +0200

    all good

---
+++ simple-js-app/JS/scripts.js
@@ -0,0 +1,22 @@
+let myName = 'Wicho';
+document.write(myName);
+myName = 'Don Wicho';
+document.write(myName);
+
+// Note: this is my first time ever I open a js file //
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+newFunction();
+function newFunction() {
+    let size = 100;
+    let doubleSize = size * 2;
+    let minSize = (doubleSize * 2) - (size / 2);
+    document.write(minSize);
+}
+

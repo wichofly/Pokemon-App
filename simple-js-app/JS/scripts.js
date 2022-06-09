8ab347f1c1441bc1a9ae174c76f46f8ef07f54b8
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-09 10:22:08 +0200

    Adding Pokemon objects
    I created 5 objects in  the pokemonList.

---
+++ simple-js-app/JS/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Posion'] },
+    { name: 'Phyduck', height: 0.8, type: 'Water' },
+    { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying'] },
+    { name: 'Onix', height: 8.8, type: ['Rock', 'Ground'] },
+    { name: 'Gangar', height: 1.5, type: ['Ghost', 'Posion']},
+]

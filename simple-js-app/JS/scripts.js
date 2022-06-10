e899fcf2adf3f7325240189be7feff340d057615
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-10 15:56:37 +0200

    adding <br>

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -4,19 +4,19 @@ let pokemonList = [
     { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying']},
     { name: 'Onix', height: 8.8, type: ['Rock', 'Ground'] },
     { name: 'Gangar', height: 1.5, type: ['Ghost', 'Posion']},
-    { name: 'Picachu', height: 1.0, type: 'Electrcity'}
+    { name: 'Picachu', height: 1.0, type: 'Electric'}
 ];
 
 for (let i = 0; i < pokemonList.length; i++){
     if(pokemonList[i].height > 1.0 && pokemonList[i].height < 2.5 ){
-        document.write(pokemonList[i].name + ` Average pokemon `)
+        document.write(pokemonList[i].name + ` Average pokemon ` + '<br>')
     } else if( pokemonList[i].height <= 1.0){
-        document.write(pokemonList[i].name + ` Small pokemon `)
+        document.write(pokemonList[i].name + ` Small pokemon `+ '<br>')
     } else {
-        document.write(pokemonList[i].name + ` Big pokemon `)
+        document.write(pokemonList[i].name + ` Big pokemon `+ '<br>')
     }
     if(pokemonList[i].height > 5.0) {
-        document.write(pokemonList[i].name + ` Wow, that's big! `)
+        document.write(pokemonList[i].name + ` Wow, that's big! `+ '<br>')
     }
 }
 

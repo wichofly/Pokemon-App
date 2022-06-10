8cf0830df390ea1bbdb4dad428789ff0bc37f201
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-10 15:40:57 +0200

    Adding loops
    I added loop (for) to identify which of my Pokémons are Small, average and big according to their height

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -1,7 +1,20 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Posion'] },
-    { name: 'Phyduck', height: 0.8, type: 'Water' },
-    { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying'] },
+    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Posion']},
+    { name: 'Phyduck', height: 0.8, type: 'Water'},
+    { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying']},
     { name: 'Onix', height: 8.8, type: ['Rock', 'Ground'] },
     { name: 'Gangar', height: 1.5, type: ['Ghost', 'Posion']},
-]
+    { name: 'Picachu', height: 1.0, type: 'Electrcity'}
+];
+
+for (let i = 0; i < pokemonList.length; i++){
+    if(pokemonList[i].height > 1.0 && pokemonList[i].height < 2.5 ){
+        document.write(pokemonList[i].name + ` Average pokemon `)
+    } else if( pokemonList[i].height <= 1.0){
+        document.write(pokemonList[i].name + ` Small pokemon `)
+    } else {
+        document.write(pokemonList[i].name + ` Big pokemon `)
+    }
+}
+
+

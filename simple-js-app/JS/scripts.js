25b26a8a24aff67d0a098069aa967f129f9a8519
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-12 08:27:51 +0200

    Recommendation for my tutor
    adding parenthesis in the height and applying the String Template Literals.

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -11,10 +11,13 @@ let pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++){
     if(pokemonList[i].height >= 3.0) {
-       document.write(pokemonList[i].name + ` height ` + pokemonList[i].height + ` Big` + ` -Wow, that's big!` + ` <br>`)
+
+        // An easier way to do it is String Template Literals, with back ticks (` `)
+        // and do not forget the variable inside of ${}     
+       document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! <br>`)
     } else if(pokemonList[i].height > 1.0 && pokemonList[i].height < 3.0){
-        document.write(pokemonList[i].name + ` height ` + pokemonList[i].height + ` Medium` + ` <br>`)
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Medium <br>`)
     } else {
-        document.write(pokemonList[i].name + (` height ` + pokemonList[i].height) + ` Small` + ` <br>`)
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Small <br>`)
     }
 }

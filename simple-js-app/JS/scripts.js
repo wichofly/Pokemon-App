5e60fcb3c3bbafb59393a4bcb09462012bfe2195
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-14 20:09:14 +0200

    creating IIFE
    i renamed all the variable pokemonList inside to privatePokemonList. so i dont get confused with the data.
    
    to make the ForEach loop work, create a new variable "pokemonList" by extracting the result of pokemonRepository.getAll()

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -1,11 +1,27 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Posion']},
-    { name: 'Phyduck', height: 0.8, type: 'Water'},
-    { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying']},
-    { name: 'Onix', height: 8.8, type: ['Rock', 'Ground'] },
-    { name: 'Gangar', height: 1.5, type: ['Ghost', 'Posion']},
-    { name: 'Picachu', height: 0.4, type: 'Electric'}
-];
+let pokemonRepository = (function () {
+    let privatePokemonList = [
+        { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Posion'] },
+        { name: 'Phyduck', height: 0.8, type: 'Water' },
+        { name: 'Dragonite', height: 2.2, type: ['Dragon', 'Flying'] },
+        { name: 'Onix', height: 8.8, type: ['Rock', 'Ground'] },
+        { name: 'Gangar', height: 1.5, type: ['Ghost', 'Posion'] },
+        { name: 'Picachu', height: 0.4, type: 'Electric' }
+    ]
+
+    function add(pokemon){
+        privatePokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return privatePokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+    };
+    
+})();
 
 // "lenght"= gives the number of items in the Array. until now are 6 items
 
@@ -24,12 +40,15 @@ for (let i = 0; i < pokemonList.length; i++){
 }
 */
 
-pokemonList.forEach(function(pokemon){
-    if(pokemon.height >= 3.0){
+let pokemonList= pokemonRepository.getAll();
+
+
+pokemonList.forEach(function (pokemon) {
+    if (pokemon.height >= 3.0) {
         document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow, thats's big! <br>`)
-    } else if(pokemon.height > 1.0 && pokemon.height < 3.0){
-        document.write(`${pokemon.name} (height: ${pokemon.height}) - Medium <br>`)   
-    } else{
-        document.write(`${pokemon.name} (height: ${pokemon.height}) - Small <br>`)   
+    } else if (pokemon.height > 1.0 && pokemon.height < 3.0) {
+        document.write(`${pokemon.name} (height: ${pokemon.height}) - Medium <br>`)
+    } else {
+        document.write(`${pokemon.name} (height: ${pokemon.height}) - Small <br>`)
     }
 })

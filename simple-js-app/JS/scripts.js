6e9a8a438541a62ac0e474beba49e2445aca59dd
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-21 11:18:48 +0200

    what I couldn't show is the image I have saved in my img.file with the name ash.png

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -36,8 +36,11 @@ let pokemonRepository = (function () {
 
     let container = document.querySelector('#container');
     let newImage = document.createElement('img');
-    newImage.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-    container.appendChild(newImage)
+    let newAsh = document.createElement('img');
+    newImage.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+    newAsh.src = 'ash.png';
+    container.appendChild(newImage);
+    container.appendChild(newAsh);
 
     function showDetails(pokemon) {
         console.log(pokemon);

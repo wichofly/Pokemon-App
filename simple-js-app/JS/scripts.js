26e18a41702f2d95925b026347b7a5c8662d1328
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-22 17:57:53 +0200

    adding showModal function

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -97,6 +97,15 @@ let pokemonRepository = (function () {
         });
     }
 
+    function showModal() {
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.add('is-visible');
+      }
+      
+      document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal();
+      });
+
     return {
         add: add,
         getAll: getAll,

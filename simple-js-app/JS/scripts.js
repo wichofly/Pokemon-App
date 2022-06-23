66b83e152e42be5c76a2f1c86878dffa9fba416f
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-23 11:23:40 +0200

    Revert "Trying another Loop"
    This reverts commit 9074128918fb711f1fc869b28be6653b33dbd5d8.

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -85,9 +85,7 @@ let pokemonRepository = (function () {
             item.imageUrl = details.sprites.other.dream_world.front_default;
             item.height = details.height;
             let types = [];
-			details.types.forEach(function(item) {
-                types.push(item.type.name)
-            });
+			details.types.forEach((item) => types.push(item.type.name));
 			item.types = types;
             item.weight = details.weight;
         }).catch(function (e) {
@@ -126,7 +124,7 @@ let pokemonRepository = (function () {
         heightElement.innerText = `Height: ${pokemon.height}`;
                                     
         let weightElement = document.createElement('p');
-        weightElement.innerText = `Weight: ${pokemon.weight}`;
+        weightElement.innerText = `Weight: ${pokemon.weight} Kg`;
 
         let typesElement = document.createElement('p');
 		typesElement.innerText = `Types: ${pokemon.types}`;

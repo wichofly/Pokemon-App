98393bcbfd79973abeb8c23f5917b992896c792b
Mauricio Erazo <antonierazo13@hotmail.com>
2022-06-30 16:22:14 +0200

    deleting comments

--- JS/scripts.js
+++ JS/scripts.js
@@ -33,16 +33,6 @@ let pokemonRepository = (function () {
         eventListener(button, pokemon);
     };
 
-    /* Adding img 
-    let container = document.querySelector('#container');
-    let newImage = document.createElement('img');
-    let newAshImg = document.createElement('img');
-    newImage.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-    newAshImg.src = 'img/ash.png';
-    container.appendChild(newImage)
-    container.appendChild(newAshImg)
-    */
-
     function eventListener(button, pokemon) {
         button.addEventListener('click', function () {
             showDetails(pokemon)
@@ -239,34 +229,3 @@ pokemonRepository.loadList().then(function () {
     });
 });
 
-
-/*
-for (let i = 0; i < pokemonList.length; i++){
-    if(pokemonList[i].height >= 3.0) {
-
-        // An easier way to do it is String Template Literals, with back ticks (` `)
-        // and do not forget the variable inside of ${}     
-       document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! <br>`)
-    } else if(pokemonList[i].height > 1.0 && pokemonList[i].height < 3.0){
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Medium <br>`)
-    } else {
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Small <br>`)
-    }
-}
-*/
-
-
-/*
-=============================================================================
-We are going to use the Loop with addListItem(), is not more needed this Loop
-=============================================================================
-pokemonList.forEach(function (pokemon) {
-    if (pokemon.height >= 3.0) {
-        document.write(`${pokemon.name} (height: ${pokemon.height}) - Wow, thats's big! <br>`)
-    } else if (pokemon.height > 1.0 && pokemon.height < 3.0) {
-        document.write(`${pokemon.name} (height: ${pokemon.height}) - Medium <br>`)
-    } else {
-        document.write(`${pokemon.name} (height: ${pokemon.height}) - Small <br>`)
-    }
-})
-*/
